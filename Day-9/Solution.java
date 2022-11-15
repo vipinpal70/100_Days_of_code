20185fea1575cc9f86a6a5fc6d7d2f901f024d0b
Vipin <64849587+vipinpal70@users.noreply.github.com>
2022-11-15 18:36:32 +0530

    Create Solution.java

---
+++ Day-9/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. 
+        Scanner s = new Scanner(System.in);
+        int b = s.nextInt();
+        int h = s.nextInt();
+        if( (b>=0 && b<=100) && (h>=0 && h<=100) ){
+            System.out.println(b*h);
+        }
+        else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+}
