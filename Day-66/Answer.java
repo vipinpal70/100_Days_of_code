47d6d2ffcbd69d029b47980b3ce7dc810fa13461
Vipin <64849587+vipinpal70@users.noreply.github.com>
2023-01-12 19:55:32 +0530

    Create Answer.java

---
+++ Day-66/Answer.java
@@ -0,0 +1,43 @@
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.MissingNumber(array, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+ 
+
+class Solution {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+        int res = 0;
+        for(int i=1;i<=n; i++)
+            res = res^i;
+            
+        for(int j=0; j<n-1; j++)
+            res = res^array[j];
+        
+        return res;
+    }
+}
+
+
+ 
