2f4a27253781327e0b2876b15a5d2976eb0e2b39
Vipin <64849587+vipinpal70@users.noreply.github.com>
2023-01-26 22:27:53 +0530

    Create Answer.java

---
+++ Day-79/Answer.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int M = Integer.parseInt(S[1]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.closestNumber(N,M));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+ 
+
+class Solution {
+    static int closestNumber(int N , int M) {
+        // code
+        float q = Math.round(N / (float)M);
+
+    return (int)(q * M);
+    }
+};
