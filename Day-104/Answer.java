99edf6324a6563b53a10add37d9dc1cfda9022e7
Vipin <64849587+vipinpal70@users.noreply.github.com>
2023-02-22 22:20:59 +0530

    Create Answer.java

---
+++ Day-104/Answer.java
@@ -0,0 +1,55 @@
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] ar, int n) {
+    {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    // Calculate square of array elements
+                    int x = ar[i] * ar[i], y = ar[j] * ar[j], z = ar[k] * ar[k];
+ 
+                    if (x == y + z || y == x + z || z == x + y)
+                        return true;
+                }
+            }
+        }
+ 
+        // If we reach here, no triplet found
+        return false;
+    }
+ 
+    }
+}
+
