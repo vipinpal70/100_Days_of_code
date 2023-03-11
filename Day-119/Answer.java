44bccb796f985f629690bdc6d1ddd7cf8e518518
Vipin <64849587+vipinpal70@users.noreply.github.com>
2023-03-11 23:48:36 +0530

    Create Answer.java

---
+++ Day-119/Answer.java
@@ -0,0 +1,29 @@
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Scanner;
+
+public class JavaMD5 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    	try {
+    		Scanner in = new Scanner(System.in);
+        
+    		MessageDigest md5 = MessageDigest.getInstance("MD5");
+        
+    		byte[] bytes = md5.digest(in.nextLine().getBytes());
+        
+    		in.close();
+        
+    		System.out.printf("%32x\n", new BigInteger(1, bytes));
+    				
+		} catch (Exception e) {
+        
+		e.printStackTrace();
+        
+		}
+      
+    }
+}
+
